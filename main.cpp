2260dce2ae894dafbeb2bb025a2bd16e5590c872
Du Park <dupark3@gmail.com>
2018-02-28 07:01:32 -0500

    Added comments and #included median header file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <vector>
 
+#include "median.h"
+
 int main() {
     std::cout << "Enter numbers to find median of: ";
+
+    // store input in vector one by one
     std::vector<int> vec;
     int n;
     while (std::cin >> n)
         vec.push_back(n);
 
-    std::cout<< "Median of those numbers are: " << median(vec);
+    // print result of median function
+    std::cout << "Median of those numbers are: " << median(vec);
 
     return 0;
 }
