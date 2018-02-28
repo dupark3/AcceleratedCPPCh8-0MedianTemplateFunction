ca8d15dda2f1d1b539bc2a1f616645a82f1071f7
Du Park <dupark3@gmail.com>
2018-02-28 07:00:32 -0500

    Median header (delcaration) and hpp (definition) created. Header #includes the .hpp file with the implementation

---
+++ median.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_median_h
+#define GUARD_median_h
+
+#include <vector>
+
+#include "median.hpp"
+
+template <class T>
+T median(std::vector<T>&);
+
+#endif // GUARD_median_h
---
+++ median.hpp
@@ -0,0 +1,18 @@
+#include <algorithm> // sort
+#include <stdexcept> // domain_error
+#include <vector>
+
+template <class T>
+T median (std::vector<T>& vec){
+    typedef typename std::vector<T>::size_type vec_size;
+    vec_size size = vec.size();
+
+    if (size == 0)
+        throw std::domain_error("Median of an empty vector");
+
+    sort(vec.begin(), vec.end());
+
+    vec_size mid = size / 2;
+
+    return size % 2 == 0 ? (vec[mid - 1] + vec[mid]) / 2 : vec[mid];
+}
