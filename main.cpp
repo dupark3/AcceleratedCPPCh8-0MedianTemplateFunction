8ebc937e85aa3591bdd32a2adf93ac074d4f63f9
Du Park <dupark3@gmail.com>
2018-02-28 07:20:40 -0500

    Added test for passing a vector of doubles

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
+#include <ios> // clear
 #include <iostream>
 #include <vector>
 
 #include "median.h"
 
 int main() {
-    std::cout << "Enter numbers to find median of: ";
+    // Try passing vector of integers to median function
+    std::cout << "Enter integers to find the median of: ";
 
     // store input in vector one by one
     std::vector<int> vec;
@@ -13,7 +15,22 @@ int main() {
         vec.push_back(n);
 
     // print result of median function
-    std::cout << "Median of those numbers are: " << median(vec);
+    std::cout << "Median of those numbers are: " << median(vec) << std::endl;
+
+
+
+    // Now try passing vector of doubles to median function
+    std::cin.clear();
+    std::cout << "Enter doubles to find the median of: ";
+
+    // store input in vector
+    std::vector<double> vec_double;
+    double d;
+    while (std::cin >> d)
+        vec_double.push_back(d);
+
+    // print result of median(vector<double>)
+    std::cout << "Median of these doubles are: " << median(vec_double) << std::endl;
 
     return 0;
 }
