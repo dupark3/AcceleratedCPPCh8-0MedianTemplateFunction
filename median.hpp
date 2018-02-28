525b534c828d8262b7b677fe0368a53b805cedc2
Du Park <dupark3@gmail.com>
2018-02-28 07:16:57 -0500

    Added comments about Type dependency on operators

--- median.hpp
+++ median.hpp
@@ -10,9 +10,11 @@ T median (std::vector<T>& vec){
     if (size == 0)
         throw std::domain_error("Median of an empty vector");
 
+    // Type T must support operator<
     sort(vec.begin(), vec.end());
 
     vec_size mid = size / 2;
 
+    // Type T must support operator+ and operator/
     return size % 2 == 0 ? (vec[mid - 1] + vec[mid]) / 2 : vec[mid];
 }
